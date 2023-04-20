668c22c4acbcfd767508586c3a78d11a3bf3bf56
Simon Laden <simon.laden@payara.fish>
2023-04-20 14:52:41 +0100

    FISH-6648 - debug to see the results 7
    Signed-off-by: Simon Laden <simon.laden@payara.fish>

--- mail/run.sh
+++ mail/run.sh
@@ -62,13 +62,13 @@ fi
 
 # fix broken running script
 # add missing argument with path
-sed -i 's/echo "1 /echo "1 html /g' "$WORKSPACE/docker/run_mailtck.sh"
+# sed -i 's/echo "1 /echo "1 html /g' "$WORKSPACE/docker/run_mailtck.sh"
 # create separate directory with pluggability report
-sed -ni 'p; s|mkdir -p $WORKSPACE/results/junitreports|mkdir -p $WORKSPACE/results/junitreports-pluggability|p' "$WORKSPACE/docker/run_mailtck.sh"
+# sed -ni 'p; s|mkdir -p $WORKSPACE/results/junitreports|mkdir -p $WORKSPACE/results/junitreports-pluggability|p' "$WORKSPACE/docker/run_mailtck.sh"
 # run the converter again for pluggability report
-sed -ni 'p; s|args.txt $JT_REPORT_DIR $WORKSPACE/results/junitreports/|args.txt ${JT_REPORT_DIR}-Pluggability $WORKSPACE/results/junitreports-pluggability/|p' "$WORKSPACE/docker/run_mailtck.sh"
+# sed -ni 'p; s|args.txt $JT_REPORT_DIR $WORKSPACE/results/junitreports/|args.txt ${JT_REPORT_DIR}-Pluggability $WORKSPACE/results/junitreports-pluggability/|p' "$WORKSPACE/docker/run_mailtck.sh"
 # tar both results
-sed -i 's|^tar \(.*\)$|tar \1 ${JT_REPORT_DIR}-Pluggability $WORKSPACE/results/junitreports-pluggability|g' "$WORKSPACE/docker/run_mailtck.sh"
+# sed -i 's|^tar \(.*\)$|tar \1 ${JT_REPORT_DIR}-Pluggability $WORKSPACE/results/junitreports-pluggability|g' "$WORKSPACE/docker/run_mailtck.sh"
 
 
 
@@ -86,4 +86,4 @@ report=$SCRIPTPATH/../results/mail-$TIMESTAMP.tar.gz
 echo Creating report $report
 # tar zcf $report $WORKSPACE/payara6/glassfish/domains/domain1/logs
 # tar zcf $report $WORKSPACE/mail-tck/JTreport $WORKSPACE/mail-tck/JTreport-Pluggability $WORKSPACE/results
-tar zcf $report $WORKSPACE/mail-tck/JTreport $WORKSPACE/results
+tar zcf $report $WORKSPACE/results
