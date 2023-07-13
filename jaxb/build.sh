071700bf9c2d7ea8c74e98035c359ee8321caedc
Simon Laden <simon.laden@payara.fish>
2023-07-13 15:53:52 +0100

    FISH-6732 - attempt to remove 3/4 of the tests
    Signed-off-by: Simon Laden <simon.laden@payara.fish>

--- jaxb/build.sh
+++ jaxb/build.sh
@@ -36,3 +36,7 @@ fi
 sed -i "s/glassfish7/payara6/g" "$WORKSPACE/docker/build_jaxbtck.sh"
 
 bash -x $WORKSPACE/docker/build_jaxbtck.sh
+
+rm -rf $WORKSPACE/jaxb-tck/tests/java2schema
+rm -rf $WORKSPACE/jaxb-tck/tests/schema_bind
+rm -rf $WORKSPACE/jaxb-tck/tests/schema2java
